146836630ca23dd153beb7f4fd141d5d93e27135
Jordan Leavitt <jordanleavitt1992@gmail.com>
2018-09-29 22:02:30 -0600

    Initial Commit

---
+++ ToDoIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoIt
+//
+//  Created by Jordan Leavitt on 9/29/18.
+//  Copyright © 2018 Jordan Leavitt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
